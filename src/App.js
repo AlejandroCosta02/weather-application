3cb2a4b19c50bdde12e48b5cd0ee959c23794f02
“Alejandro <costa.claudio.alejandro@gmail.com>
2023-02-16 14:29:37 -0300

    font size details

--- src/App.js
+++ src/App.js
@@ -46,16 +46,16 @@ function App() {
         <div className="bottom">
           <div className="feels">
             {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°C</p> : null}
-            <p>Feels Like</p>
+            <p className="detail">Feels Like</p>
           </div>
           <div className="humidity">
             {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
 
-            <p>Humidity</p>
+            <p className="detail">Humidity</p>
           </div>
           <div className="wind">
             {data.wind ? <p className="bold">{data.wind.speed.toFixed()} MPH</p> : null}
-            <p>Wind</p>
+            <p className="detail">Wind</p>
           </div>
         </div>
         }
